6f529eddaffca2372da779ef8df8911317daea2e
etherusko <etherusko.labs@gmail.com>
2022-08-23 03:14:53 -0500

    Funciones de los botones Borrar y Crear-Nuevo completadas

--- temporizador.js
+++ temporizador.js
@@ -1,4 +1,5 @@
 const contenedor = document.getElementById("display");
+let blockId = 0;
 
 function btnPlayOnClick(){
     console.log("Se ha pulsado el botón Play");
@@ -6,21 +7,36 @@ function btnPlayOnClick(){
 function btnReiniciarOnClick(){
     console.log("Se ha pulsado el botón Reiniciar");
 }
-function btnBorrarOnClick(){
+function btnBorrarOnClick(e){
+    contenedor.removeChild(e);
     console.log("Se ha pulsado el botón Borrar");
 }
+
+/**
+ * Botón Crear Nuevo
+ */ 
 function btnCrearNuevoOnClick(){
-    let block = document.createElement('div');
-    let displayTime = document.createElement('div');
-    let cuentaRegresiva = document.createElement('p'); 
-    block.setAttribute('class','block') 
-    displayTime.setAttribute('class', 'display-time');
-    cuentaRegresiva.innerText="Un texto simpaticon";
-
-    displayTime.appendChild(cuentaRegresiva);
-    block.appendChild(displayTime);
-    contenedor.appendChild(block);
-
+    blockId+=1;
+    crearContador();
     console.log("Se ha pulsado el botón Crear Nuevo");
 }
-
+function crearContador(){
+    let block = crearElementos('div',['class','block'],contenedor,"");
+    block.setAttribute('id',blockId);
+    let displayTime = crearElementos('div',['class','display-time'],block,"");
+    let displayButtons = crearElementos('div',['class','display-buttons'],block,"");
+    let cuentaRegresiva = crearElementos('p',[],displayTime,"Un buen pedazo de texto");
+    let btnPlay = crearElementos('button',['class', 'display-btn'],displayButtons,"Play");
+    btnPlay.setAttribute('onclick','btnPlayOnClick()');
+    let btnReiniciar = crearElementos('button',['class', 'display-btn'],displayButtons,"Reiniciar");
+    btnReiniciar.setAttribute('onclick','btnReiniciarOnClick()');
+    let btnBorrar = crearElementos('button',['class', 'display-btn'],displayButtons,"Borrar");
+    btnBorrar.setAttribute('onclick',`btnBorrarOnClick(document.getElementById(${blockId}))`); 
+}
+function crearElementos(etiqueta,[atributo,valor],padre,texto){
+    let element =document.createElement(etiqueta);
+    element.setAttribute(atributo,valor);
+    element.innerText = texto;
+    padre.appendChild(element);
+    return element;
+}
