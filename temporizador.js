049a3a4f8072766ff016938eb4d72537877e3fcb
etherusko <etherusko.labs@gmail.com>
2022-08-22 22:16:24 -0500

    creacion del archivo .js y funciones para escuchar los botones

---
+++ temporizador.js
@@ -0,0 +1,23 @@
+const contenedor = document.getElementById("temp-0");
+
+function btnPlayOnClick(){
+    console.log("Se ha pulsado el botón Play");
+}
+function btnReiniciarOnClick(){
+    console.log("Se ha pulsado el botón Reiniciar");
+}
+function btnBorrarOnClick(){
+    console.log("Se ha pulsado el botón Borrar");
+}
+function btnCrearNuevoOnClick(){
+
+    /*let displayTime = document.createElement('div');
+    let cuentaRegresiva = document.createElement('p');
+    displayTime.setAttribute('class', 'display-time');
+    cuentaRegresiva.innerText="Un texto simpaticon";
+    displayTime.appendChild(cuentaRegresiva);
+    contenedor.appendChild(displayTime);*/
+
+    console.log("Se ha pulsado el botón Crear Nuevo");
+}
+
