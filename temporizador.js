ee22228906ba36e7ae11d3f9c9223c3a7366d1f7
etherusko <etherusko.labs@gmail.com>
2022-08-24 01:23:12 -0500

    Display de cuenta regresiva y mejoras en las funciones de los botones

--- temporizador.js
+++ temporizador.js
@@ -1,42 +1,75 @@
 const contenedor = document.getElementById("display");
 let blockId = 0;
-
-function btnPlayOnClick(){
+function btnPlayOnClick(e){
+    let thisButton = e.children[1].children[0];
+    thisButton.innerText = thisButton.innerText == "Play" ? "Pause" : "Play";
     console.log("Se ha pulsado el botón Play");
 }
 function btnReiniciarOnClick(){
     console.log("Se ha pulsado el botón Reiniciar");
 }
+
+/**
+ * Botón Borrar
+ */
 function btnBorrarOnClick(e){
     contenedor.removeChild(e);
+    clearInterval(e.id);    
     console.log("Se ha pulsado el botón Borrar");
 }
-
 /**
  * Botón Crear Nuevo
  */ 
 function btnCrearNuevoOnClick(){
     blockId+=1;
-    crearContador();
+    let miliseconds = leerInputs();
+    let timeMark = new Date();
+    crearContador(miliseconds,timeMark);
     console.log("Se ha pulsado el botón Crear Nuevo");
 }
-function crearContador(){
-    let block = crearElementos('div',['class','block'],contenedor,"");
-    block.setAttribute('id',blockId);
-    let displayTime = crearElementos('div',['class','display-time'],block,"");
-    let displayButtons = crearElementos('div',['class','display-buttons'],block,"");
-    let cuentaRegresiva = crearElementos('p',[],displayTime,"Un buen pedazo de texto");
-    let btnPlay = crearElementos('button',['class', 'display-btn'],displayButtons,"Play");
-    btnPlay.setAttribute('onclick','btnPlayOnClick()');
-    let btnReiniciar = crearElementos('button',['class', 'display-btn'],displayButtons,"Reiniciar");
-    btnReiniciar.setAttribute('onclick','btnReiniciarOnClick()');
-    let btnBorrar = crearElementos('button',['class', 'display-btn'],displayButtons,"Borrar");
-    btnBorrar.setAttribute('onclick',`btnBorrarOnClick(document.getElementById(${blockId}))`); 
+function leerInputs(){
+    let dias = document.getElementById("inputDays").value*3600*24;
+    let horas = document.getElementById("inputHours").value*3600;
+    let minutos = document.getElementById("inputMinuts").value*60;
+    let segundos = document.getElementById("inputSeconds").value*1;
+    let seconds = dias+horas+minutos+segundos;
+    return seconds*1000;
 }
-function crearElementos(etiqueta,[atributo,valor],padre,texto){
-    let element =document.createElement(etiqueta);
+function crearContador(miliseconds,timeMark){
+    let displayElement=crearElementos();
+    setInterval(function(){
+        let tiempoRestante = timeMark-new Date()+miliseconds;
+        displayElement.innerText = calcularDisplay(tiempoRestante);
+        console.log(tiempoRestante);
+    },1000);
+}
+function crearElementos(){
+    let block = crearElemento('div',['class','block'],contenedor,"");
+    block.setAttribute('id',blockId);
+    let displayTime = crearElemento('div',['class','display-time'],block,"");
+    let displayButtons = crearElemento('div',['class','display-buttons'],block,"");
+    let cuentaRegresiva = crearElemento('p',[],displayTime,"Problemas aca");
+    let btnPlay = crearElemento('button',['class', 'display-btn'],displayButtons,"Play");
+    btnPlay.setAttribute('id','btnPlay'+blockId)
+    btnPlay.setAttribute('onclick',`btnPlayOnClick(document.getElementById(${blockId}))`);
+    let btnReiniciar = crearElemento('button',['class', 'display-btn'],displayButtons,"Reiniciar");
+    btnReiniciar.setAttribute('onclick','btnReiniciarOnClick()');
+    let btnBorrar = crearElemento('button',['class', 'display-btn'],displayButtons,"Borrar");
+    btnBorrar.setAttribute('onclick',`btnBorrarOnClick(document.getElementById(${blockId}))`);
+    return cuentaRegresiva;
+}
+function crearElemento(etiqueta,[atributo,valor],padre,texto){
+    let element = document.createElement(etiqueta);
     element.setAttribute(atributo,valor);
     element.innerText = texto;
     padre.appendChild(element);
     return element;
+}
+function calcularDisplay(tiempo){
+	let dias = Math.floor(Math.abs(tiempo/1000/3600/24));
+	let horas = Math.floor(Math.abs(tiempo/1000/3600) % 24);
+	let minutos = Math.floor(Math.abs(tiempo/1000/60) % 60);
+	let segundos = Math.floor(Math.abs(tiempo/1000) % 60);
+    let signo = tiempo >= 0 ? "" : "-"
+	return signo+dias+" Días	"+signo+horas+" h : "+signo+minutos+" min : "+signo+segundos+" sec";
 }
